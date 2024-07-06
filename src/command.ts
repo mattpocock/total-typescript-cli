fd1f0382570f90c240431cad67be8a7c4377ab4e
Matt Pocock <mattpocockvoice@gmail.com>
2024-07-06 07:37:30 +0100

    Removed old commands: prune and upgrade

--- src/command.ts
+++ src/command.ts
@@ -5,9 +5,7 @@ import {
   compareSnapshotAgainstExisting,
   takeSnapshot,
 } from "./snapshotExercises";
-import { upgrade } from "./upgrade";
 import { runPrompts } from "./runPrompts";
-import { prune } from "./prune";
 import packageJson from "../package.json" with { type: "json" };
 import { createSectionRepos } from "./create-section-repos";
 
@@ -36,11 +34,6 @@ totalTypeScriptCLI
   );
 
 totalTypeScriptCLI
-  .command("prune <exercise>")
-  .description("Removes all exercise files except for the one specified")
-  .action(prune);
-
-totalTypeScriptCLI
   .command("create-section-repos")
   .description("Creates section repos")
   .action(createSectionRepos);
@@ -59,17 +52,3 @@ totalTypeScriptCLI
   .command("compare-snapshot <snapshotPath>")
   .description("Compares the current state of the exercises against a snapshot")
   .action(compareSnapshotAgainstExisting);
-
-totalTypeScriptCLI
-  .command("upgrade")
-  .description(
-    "Upgrades TypeScript, Vitest and the TT CLI to the latest version, with snapshot tests.",
-  )
-  .action(() => upgrade("latest"));
-
-totalTypeScriptCLI
-  .command("upgrade-beta")
-  .description(
-    "Upgrades TypeScript to the beta version, Vitest and the TT CLI to the latest versions, with snapshot tests.",
-  )
-  .action(() => upgrade("beta"));
